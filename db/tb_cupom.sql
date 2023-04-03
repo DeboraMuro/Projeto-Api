ea3f1932dfa6ef1315e6ea77c3f7a662287de593
unknown <julia19santos91@gmail.com>
2023-04-03 20:21:06 -0300

    criando tb cupom

---
+++ db/tb_cupom.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tb_cupom (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    codigo VARCHAR (50) NOT NULL,
+    descricao VARCHAR (255)
+);
---
+++ resources/marca/routes.js
@@ -0,0 +1,9 @@
+// const app = require('express').Router();
+// const database = require('../../connection/database');
+
+// app.get('/marca', async (req, res) => {
+
+//     res.send(dados);
+// })
+
+// module.exports = app;
