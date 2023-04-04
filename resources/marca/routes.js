74111266ae8698a61462753c38a596588cb90180
unknown <julia19santos91@gmail.com>
2023-04-04 19:05:36 -0300

    criando tabela de carrinho

--- resources/marca/routes.js
+++
@@ -1,9 +0,0 @@
-// const app = require('express').Router();
-// const database = require('../../connection/database');
-
-// app.get('/marca', async (req, res) => {
-
-//     res.send(dados);
-// })
-
-// module.exports = app;
