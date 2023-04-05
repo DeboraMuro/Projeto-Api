abc0962656d8eb9f40c92629cd1cdc9cfb5bfc6f
Débora Muro <100884019+DeboraMuro@users.noreply.github.com>
2023-04-04 23:15:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,9 @@
 const express = require('express');
 const categoryRoutes = require("./resources/category/routes");
 const bannerRoutes = require('./resources/banner/routes');
-
-
-const express = require ('express');
-const categoryRoutes = require ("./resources/category/routes");
-const bannerRoutes = require ("./resources/banner/routes");
 const colecoesRoutes = require ( "./resources/colecoes_destaque/routes");
 const carrinhoRoutes = require('./resources/carrinho/routes');
-const marcaRouts = require('./resources/marca/routes');
+const marcaRoutes = require('./resources/marca/routes');
 
 const app = express();
 const cors= require ('cors');
@@ -19,8 +14,9 @@ app.use (cors());
 app.use(express.json());
 app.use(categoryRoutes);
 app.use(bannerRoutes);
+app.use(colecoesRoutes);
 app.use(carrinhoRoutes);
-app.use(marcaRouts);
+app.use(marcaRoutes);
 
 app.listen(8000, () => {
     console.log('--------------');
