69810825a74d7a0b3331198fbbf594d40c4b64da
Debora Muro <debora.flaviabm@hotmail.com>
2023-04-06 12:02:32 -0300

    aterando tabelas

--- db/tb_endereco.sql
+++ db/tb_endereco.sql
@@ -7,5 +7,11 @@ CREATE TABLE tb_endereco (
     cidade VARCHAR(20) NOT NULL,
     cep INT NOT NULL,
     estado VARCHAR(20) NOT NULL,
-    cliente_id  INT NOT NULL,
+    cliente_id  INT
+    FOREIGN KEY (cliente_id) REFERENCES tb_cliente(id),
 );
+
+-- INSERT INTO tb_endereco (logradouro, numero, complemento, bairro, cidade, cep, estado)
+-- VALUES ('av Rua Padre Roma', 1150, 'casa', 'fatima', 'fortaleza', 60040360, 'CE')
+
+-- acrescentar no insomnia
