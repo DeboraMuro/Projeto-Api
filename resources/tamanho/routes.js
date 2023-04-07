d546b94cf9f92d808de070e71480769bf96abdb8
DeboraMuro <debora.flaviabm@hotmail.com>
2023-04-07 16:01:03 -0300

    corrigindo resources e tabelas

--- resources/tamanho/routes.js
+++ resources/tamanho/routes.js
@@ -5,77 +5,59 @@ const TABLE_NAME = 'tb_tamanhos'
 const BASE_URL = '/tamanhos'
 
 app.get(BASE_URL, async (req, res) => {
-    // let dados = await database.execute(`SELECT * FROM ${TABLE_NAME}`);
+    let dados = await database.execute(`SELECT * FROM ${TABLE_NAME}`);
 
-    // res.send(dados);
-    res.send('ok');
+    res.send(dados);
 });
 
 app.get(`${BASE_URL}/:id`, async (req, res) => {
-//     let dados = await database.execute(`
-//     SELECT * FROM ${TABLE_NAME} WHERE id='${req.params.id}'
-// `);
+    let dados = await database.execute(`
+    SELECT * FROM ${TABLE_NAME} WHERE id='${req.params.id}'
+`);
 
-// res.send(dados[0]);
-// });
-
-// app.post(BASE_URL, async (req, res) => {
-//     let corpo = req.body;
-
-//     let sql = await database.execute(`
-//         INSERT INTO ${TABLE_NAME} (tamanho)
-//         VALUES ('${corpo.tamanho}')
-//     `);
-
-//     corpo.id = sql.insertId;
-    
-//     res.send(corpo);
-    res.send('ok');
+res.send(dados[0]);
 });
 
 app.post(BASE_URL, async (req, res) => {
-    // let corpo = req.body;
+    let corpo = req.body;
 
-    // let sql = await database.execute(`
-    //     INSERT INTO ${TABLE_NAME} (logradouro, numero, complemento, bairro, cidade, cep, estado, cliente_id)
-    //     VALUES ('${corpo.logradouro}', '${corpo.numero}', '${corpo.complemento}', '${corpo.bairro}', '${corpo.cidade}', '${corpo.cep}', '${corpo.estado}', '${corpo.cliente_id}')
-    // `);
+    let sql = await database.execute(`
+        INSERT INTO ${TABLE_NAME} (tamanho)
+        VALUES ('${corpo.tamanho}')
+    `);
 
-    // corpo.id = sql.insertId;
+    corpo.id = sql.insertId;
     
-    // res.send(corpo);
-    res.send('ok');
+    res.send(corpo);
 });
 
 app.patch(`${BASE_URL}/:id`, async (req, res) => {
-    // let dados = req.body;
+    let dados = req.body;
 
-    // let jaExiste = await database.execute(`
-    //     SELECT FROM ${TABLE_NAME} WHERE id='${req.params.id}'
-    // `);
+    let jaExiste = await database.execute(`
+        SELECT FROM ${TABLE_NAME} WHERE id='${req.params.id}'
+    `);
 
-    // if (undefined === jaExiste[0]) {
-    //     res.sendStatus(404);
-    //     return;
-    // }
+    if (undefined === jaExiste[0]) {
+        res.sendStatus(404);
+        return;
+    }
 
-    // await database.execute(`
-    //     UPDATE ${TABLE_NAME} SET
-    //         tamanho='${req.body.tamanho || jaExiste[0].tamanho}'
-    //     WHERE id='${req.params.id}'
-    // `);
+    await database.execute(`
+        UPDATE ${TABLE_NAME} SET
+            tamanho='${req.body.tamanho || jaExiste[0].tamanho}'
+        WHERE id='${req.params.id}'
+    `);
 
-    // dados.id = req.params.id;
+    dados.id = req.params.id;
 
-    // res.send(dados);
-    res.send('ok');
+    res.send(dados);
 });
 
 app.delete(`${BASE_URL}/:id`, async (req, res) => {
-    // await database.execute(`DELETE FROM ${TABLE_NAME} WHERE id='${req.params.id}`);
+    await database.execute(`DELETE FROM ${TABLE_NAME} WHERE id='${req.params.id}`);
 
-    // res.sendStatus(204);
-    res.send('ok');
+    res.sendStatus(204);
 });
 
 module.exports = app;
