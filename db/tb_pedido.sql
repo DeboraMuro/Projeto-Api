738d9c6a07c9262b3268cc8c949a885d91ce2e2c
Egislane Gomes <egislanedago2@gmail.com>
2023-04-09 19:40:44 -0300

    Tabelas concluidas com eindpoints ok

--- db/tb_pedido.sql
+++ db/tb_pedido.sql
@@ -4,6 +4,12 @@ CREATE TABLE tb_pedido(
     cliente_id INT,
     valor_pedido VARCHAR (11),
     valor_frete VARCHAR (11),
-    cartao VARCHAR (10),
+    cartao VARCHAR (11),
     produto_id INT NOT NULL
 );
+
+INSERT INTO tb_pedido (cliente_id, valor_pedido, valor_frete, cartao, produto_id) VALUES ('1', 'R$200,00', 'R$14,90','Mastecard', '2');
+
+INSERT INTO tb_pedido (cliente_id, valor_pedido, valor_frete, cartao, produto_id) VALUES ('2', 'R$499,99', 'R$25,90','Credcard', '1');
+
+INSERT INTO tb_pedido (cliente_id, valor_pedido, valor_frete, cartao, produto_id) VALUES ('3', 'R$599,90', 'R$19,90','Elo', '3');
