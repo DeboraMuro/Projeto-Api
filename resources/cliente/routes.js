4eb5ce7fcba1e66e02e6245b009abe57a9230f1f
DeboraMuro <debora.flaviabm@hotmail.com>
2023-04-18 17:17:28 -0300

    padronizando routes cliente

--- resources/cliente/routes.js
+++ resources/cliente/routes.js
@@ -1,7 +1,7 @@
 const app = require('express').Router();
 const database = require("../../connection/database");
 
-const TABLE_NAME = 'clientes';
+const TABLE_NAME = 'tb_cliente';
 const BASE_URL = '/clientes';
 
 // Rota GET
@@ -33,11 +33,35 @@ app.post(BASE_URL, async (req, res) => {
   res.send(corpo);
 });
 
-// Rota PUT
-app.put(`${BASE_URL}/:id`, (req, res) => {
-  const id = req.params.id;
-  const { nome, email } = req.body;
-  res.send(`Cliente atualizado: ${id}, ${nome}, ${email}`);
+// Rota PATCH
+app.patch(`${BASE_URL}/:id`, async (req, res) => {
+  let dados = req.body;
+
+  let jaExiste = await database.execute(`
+        SELECT * FROM ${TABLE_NAME} WHERE id='${req.params.id}'
+    `);
+
+  if (undefined === jaExiste[0]) {
+    res.sendStatus(404);
+    return;
+  }
+
+  await database.execute(`
+    UPDATE ${TABLE_NAME} SET
+      nome='${req.body.nome || jaExiste[0].nome}'
+      email='${req.body.email || jaExiste[0].email}'
+      telefone='${req.body.telefone || jaExiste[0].telefone}'
+      endereco='${req.body.endereco || jaExiste[0].endereco}'
+      cidade='${req.body.cidade || jaExiste[0].cidade}'
+      estado='${req.body.estado || jaExiste[0].estado}'
+      cep='${req.body.cep || jaExiste[0].cep}'
+      data_cadastro='${req.body.data_cadastro || jaExiste[0].data_cadastro}'
+    WHERE id='${req.params.id}'
+  `);
+
+  dados.id = req.params.id;
+
+  res.send(dados);
 });
 
 // Rota DELETE
@@ -46,8 +70,5 @@ app.delete(`${BASE_URL}/:id`, async (req, res) => {
     
   res.sendStatus(204);
 });
-
-
-
 
 module.exports = app;
